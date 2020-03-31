d8e49ece6bc6c4e46a4939c6811a39f1be333a37
Albert Schilling <albertschilling@gmx.net>
2020-03-31 10:31:12 +0200

    Run build
    Run build again
    
    Exchange desktop image
    
    Add projects and applicant video
    
    Change styles for desktop view
    
    Bump acorn from 5.7.3 to 5.7.4
    
    Bumps [acorn](https://github.com/acornjs/acorn) from 5.7.3 to 5.7.4.
    - [Release notes](https://github.com/acornjs/acorn/releases)
    - [Commits](https://github.com/acornjs/acorn/compare/5.7.3...5.7.4)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Build
    
    Fix security issue regarding minimist
    
    Add images
    
    Create README.md
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    Update readme
    
    Exchange desktop image
    
    Add projects and applicant video
    
    Change styles for desktop view
    
    Bump acorn from 5.7.3 to 5.7.4
    
    Bumps [acorn](https://github.com/acornjs/acorn) from 5.7.3 to 5.7.4.
    - [Release notes](https://github.com/acornjs/acorn/releases)
    - [Commits](https://github.com/acornjs/acorn/compare/5.7.3...5.7.4)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Build
    
    Fix security issue regarding minimist
    
    Add images
    
    Fix security issues of dependencies
    
    Update readme
    
    Update readme

---
+++ README.md
@@ -0,0 +1,16 @@
+# Personal Website
+This is my personal website to present myself as a web developer. The site is build with **HTML** and **Vanilla JavaScript**.
+
+## URL:
+<a href="https://albert-schilling.github.io/personal-website/" target="_blank">Visit my Personal Website</a>
+
+## Mobile View:
+<img alt="Mobile View of Albert Schillings's personal website" src="https://raw.githubusercontent.com/albert-schilling/personal-website/master/src/img/personal-website-screenshot-mobile.png" width="400px" height="auto">
+
+## Desktop View:
+![Desktop View of Albert Schilling's personal website](https://raw.githubusercontent.com/albert-schilling/personal-website/master/src/img/personal-website-screenshot-desktop.png)
+
+## Contact:
+<ul><li><a href="mailto:albertschilling@gmx.net">Mail</a></li>
+<li><a href="https://www.xing.com/profile/Albert_Schilling3/" target="_blank">Xing</a></li>
+<li><a href="https://www.linkedin.com/in/albert-schilling/" target="_blank">LinkedIn</a></li></ul>
--- src/js/script.js
+++ src/js/script.js
@@ -47,23 +47,35 @@ function changeWidthIfInSight(className) {
   for (let i = 0; i < targetElementsArray.length; i++) {
     const targetElement = targetElementsArray[i]
     const targetElementParent = targetElement.parentNode
-    console.log(targetElementParent)
+    // console.log(targetElementParent)
     const heightWindow = window.innerHeight
     const elementTop = targetElementParent.getBoundingClientRect().top
-    console.log(`${elementTop} ElementTop`)
-    console.log(`${heightWindow} heightWindow`)
+    // console.log(`${elementTop} ElementTop`)
+    // console.log(`${heightWindow} heightWindow`)
     const difference = heightWindow - elementTop
-    console.log(targetElement)
-    console.log(`${difference} difference`)
+    // console.log(targetElement)
+    // console.log(`${difference} difference`)
     if (difference < 150) {
-      console.log(`Progress bar is not in sight.`)
+      // console.log(`Progress bar is not in sight.`)
       const targetElementParent = targetElement.parentNode
       targetElementParent.style.width = 0
     } else {
-      console.log(`Progress bar is in sight.`)
+      // console.log(`Progress bar is in sight.`)
       const targetElementParent = targetElement.parentNode
       const targetElementWidth = targetElement.innerHTML
       targetElementParent.style.width = targetElementWidth
     }
   }
+}
+
+const youTubeVideo = document.getElementsByClassName('youTubeVideo')[0]
+
+window.addEventListener('resize', () => adaptYouTubeVideoHeight())
+
+adaptYouTubeVideoHeight()
+
+function adaptYouTubeVideoHeight() {
+  const width = youTubeVideo.getBoundingClientRect().width
+  const height = width * 0.5625
+  youTubeVideo.height = `${height}px`
 }
